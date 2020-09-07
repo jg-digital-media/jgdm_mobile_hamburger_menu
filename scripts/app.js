ee18952d3cafe3c7f380af0194494138edf7ead5
jg-digital-media <mail@jonniegrieve.co.uk>
2020-09-07 15:33:34 +0100

    mobile menu comment

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 console.log("js-connected");
 
-// Operates mobile hamburger menu
+// Operates mobile hamburger menu  - hides on 981px or more width
 $('.ham').on("click", function(){
     $('.main_menu').slideToggle(800);
     console.log("clicked");

cea430eb45e7897a174db2a1c977a2228369fafb
jg-digital-media <mail@jonniegrieve.co.uk>
2020-09-09 11:42:15 +0100

    toggle mobile menu from left

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,10 @@ console.log("js-connected");
 
 // Operates mobile hamburger menu  - hides on 981px or more width
 $('.ham').on("click", function(){
-    $('.main_menu').slideToggle(800);
+    //$('.main_menu').slideToggle(800); 
+    //$('.main_menu').toggle("slide", "left", 800);
+    $('.main_menu').toggle({ direction: "left" }, 800);
+
     console.log("clicked");
 });
 
